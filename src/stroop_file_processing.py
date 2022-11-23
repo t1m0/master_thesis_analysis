aae29d12f2f693ce8481fa5a53a616833d8f3c26
Timo Schöpflin <timo@schoepflin.io>
2022-11-23 10:51:28 +0100

    Add stroop game support

---
+++ src/stroop_file_processing.py
@@ -0,0 +1,57 @@
+import json
+from src.file_handling import extract_subject, extract_simple_file_name
+from src.accelerometer import calc_magnitude
+
+def process_stroop_file(file_path):
+    file = open(file_path)
+    json_data = json.load(file)
+    file.close()
+    accelerations = json_data['gameSession']['accelerations']
+
+    if len(accelerations) <= 0: 
+        return None
+
+    data = []
+
+    subject = extract_subject(file_path)
+    simple_file_name = extract_simple_file_name(file_path)
+    hand = json_data['hand']
+    device = json_data['device']
+    uuid = json_data['gameSession']['uuid']
+
+    clicks = json_data['gameSession']['clicks']
+    start_time = json_data['gameSession']['startTime']
+    clicks_time_stamps = []
+    for click in clicks:
+        clicks_time_stamps.append(click['timeStamp'])
+    
+    for acceleration in accelerations:
+        click_number = 0
+        time_stamp = acceleration['timeStamp']
+        for i in range(len(clicks_time_stamps)):
+            if clicks_time_stamps[i] > time_stamp:
+                click_number = i
+            else:
+                break
+        duration = time_stamp - start_time
+        x=acceleration['xAxis']
+        y=acceleration['yAxis']
+        z=acceleration['zAxis']
+        mag=calc_magnitude(x,y,z)
+        time_stamp=acceleration['timeStamp']
+        pandas_row = {
+            'subject': subject,
+            'file': simple_file_name,
+            'uuid':uuid,
+            'hand': hand,
+            'device': device,
+            'click_number':click_number,
+            'duration':duration,
+            'time_stamp':time_stamp,
+            'x':x,
+            'y':y,
+            'z':z,
+            'mag':mag
+        }
+        data.append(pandas_row)
+    return data
