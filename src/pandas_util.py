0bb2a6606a444becb997c17bcde85f8d64e4d628
Timo Schöpflin <timo@schoepflin.io>
2023-01-12 09:31:32 +0100

    Consolidate plotting of correlation matrix

--- src/pandas_util.py
+++ src/pandas_util.py
@@ -22,9 +22,18 @@ def get_max_value_across_columns(df, fields=[]):
         min_values.append(final_df[field].max())
     return max(min_values)
 
-def drop_outliers_of_column(df, column):
-    q_max = df[column].quantile(0.90)
-    q_min = df[column].quantile(0.10)
-    df_new = df[df[column] < q_max]
-    df_new = df_new[df_new[column] > q_min]
+def drop_outliers_of_columns(df, columns):
+    df_new = df.copy()
+    for column in columns:
+        df_new = drop_outliers_of_column(df_new, column, upper_quantile=0.90)
     return df_new
+
+def drop_outliers_of_column(df, column, lower_quantile=0.10, upper_quantile=0.90):
+    q_max = df[column].quantile(upper_quantile)
+    q_min = df[column].quantile(lower_quantile)
+    df_new = df[(df[column] <= q_max) & (df[column] >= q_min)]
+    return df_new
+
+def correlation_matrix(df, cut_off_correlation=0.8):
+    correlation_matrix = df.corr()
+    return correlation_matrix.style.apply(lambda x: ["background: red" if v > cut_off_correlation or v < -cut_off_correlation else "" for v in x], axis = 1).format(precision=2)
--- src/plotting.py
+++ src/plotting.py
@@ -2,22 +2,24 @@
 
 import matplotlib.pyplot as plt
 
-from src.pandas_util import get_min_value_across_columns, get_max_value_across_columns
+from src.pandas_util import get_min_value_across_columns, get_max_value_across_columns, drop_outliers_of_columns
 
 def _box_plot_columns_single(df, class_key, columns):
+    
+    df_new = drop_outliers_of_columns(df,columns)
 
-    min_value = get_min_value_across_columns(df, columns) * 0.8
-    max_value = get_max_value_across_columns(df, columns) * 1.2
+    min_value = get_min_value_across_columns(df_new, columns) * 0.8
+    max_value = get_max_value_across_columns(df_new, columns) * 1.2
 
     if class_key != '':
         fig, ax = plt.subplots(1,2)
         plt.setp(ax, ylim=(min_value,max_value))
         fig.set_size_inches(30, 5)
-        final_df = df.groupby([class_key])[columns]
+        final_df = df_new.groupby([class_key])[columns]
         final_df.boxplot(fontsize=20, ax=ax)
     else:
         plt.ylim(min_value,max_value)
-        df[columns].boxplot(fontsize=20)
+        df_new[columns].boxplot(fontsize=20)
     
     plt.show()
 
