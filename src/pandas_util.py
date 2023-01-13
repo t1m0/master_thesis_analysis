3457cd6997843571640eca7e65c5b964ae11ee69
Timo Schöpflin <timo@schoepflin.io>
2023-01-13 08:35:13 +0100

    FIXUP WITH boxplot commit

--- src/pandas_util.py
+++ src/pandas_util.py
@@ -37,7 +37,8 @@ def get_max_value_across_columns(df, fields=[],padding=0):
 def drop_outliers_of_columns(df, columns):
     df_new = df.copy()
     for column in columns:
-        df_new = drop_outliers_of_column(df_new, column, upper_quantile=0.90)
+        if df_new[column].dtype in [float,int]:
+            df_new = drop_outliers_of_column(df_new, column, upper_quantile=0.90)
     return df_new
 
 def drop_outliers_of_column(df, column, lower_quantile=0.10, upper_quantile=0.90):
@@ -49,3 +50,12 @@ def drop_outliers_of_column(df, column, lower_quantile=0.10, upper_quantile=0.90
 def correlation_matrix(df, cut_off_correlation=0.8):
     correlation_matrix = df.corr()
     return correlation_matrix.style.apply(lambda x: ["background: red" if v > cut_off_correlation or v < -cut_off_correlation else "" for v in x], axis = 1).format(precision=2)
+
+def only_numeric_columns(df,columns = []):
+    if len(columns) == 0:
+        columns = df.columns
+    numeric_columns = []
+    for column in columns:
+        if df[column].dtype in [float,int]:
+            numeric_columns.append(column)
+    return numeric_columns
--- src/plotting.py
+++ src/plotting.py
@@ -2,32 +2,28 @@
 
 import matplotlib.pyplot as plt
 
-from src.pandas_util import get_min_value_across_columns, get_max_value_across_columns, drop_outliers_of_columns
+from src.pandas_util import drop_outliers_of_columns, only_numeric_columns
 
 def _box_plot_columns_single(df, class_key, columns, show_column_in_label):
     
     df_new = drop_outliers_of_columns(df,columns)
-    
-    plt.grid(True)
-    fig = plt.gcf()
-    fig.set_size_inches(30, 7.5)
 
     box_plot_data = {}
 
-    for column in columns:
-        if df_new[column].dtype in [float,int]:
-            if class_key != '':
-                for class_value in df_new[class_key].unique():
-                    
-                    label = f'{column} {class_value}' if show_column_in_label else class_value
-                    
-                    values = df_new[df_new[class_key]==class_value][column]
-                    box_plot_data[label] = values
-            else:
-                box_plot_data[column] = df_new[column]
+    for column in only_numeric_columns(df_new, columns):
+        if class_key != '':
+            for class_value in df_new[class_key].unique():
+                
+                label = f'{column} {class_value}' if show_column_in_label else class_value
+                
+                values = df_new[df_new[class_key]==class_value][column]
+                box_plot_data[label] = values
         else:
-            print(f"Column {column} not numeric, therefore no boxplot created!!")
+            box_plot_data[column] = df_new[column]
     if len(box_plot_data) > 0:
+        plt.grid(True)
+        fig = plt.gcf()
+        fig.set_size_inches(30, 7.5)
         plt.boxplot(list(box_plot_data.values()),labels=list(box_plot_data.keys()))
         plt.title(columns)
         plt.tight_layout()
